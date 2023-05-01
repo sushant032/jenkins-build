b926a9bad6e11d0d5887773bf325e9ee17ea9fc3
Sushant <ksushant@tekion.com>
2023-05-01 17:10:13 +0530

    added basic springboot application

---
+++ src/test/java/com/compaloon/jenkinsbuild/JenkinsBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compaloon.jenkinsbuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsBuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
